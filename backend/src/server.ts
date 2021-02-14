55186e9101615a4a40417e78bb5f88e7e9f38951
Pedro Oliveira Bayma <pedro_bayma@outlook.com>
2021-02-14 18:16:14 -0300

    Creating eslint config and add routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,8 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    response.json({ message: 'Hello typescript node, testando o salvamento' }) 
-})
-
+app.use(routes);
 
 app.listen(3333);
